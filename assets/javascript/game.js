b3c866f5428fad754bb8ee9740821b43a7313fb3
Daniele Esposito <esp.dne@gmail.com>
2019-09-29 18:05:26 +0200

    main structure ready

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,41 @@
 
+// still missing:
+// limited number of turns 10 and counting the scores and left and done guesses
 
+var wins = 0;
 
-document.onkeyup = function() {
-    alert("working");
+var losses = 0;
+
+var GuessesLeft = 0;
+
+var userguessnumb = 0;
+
+// Creates an array that lists out all of the options
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q','r','s','t','u', 'v', 'x', 'y', 'w', 'z'];
+
+// Collect the user's response and convert to lower case. (collect from the document.onkey)
+document.onkeyup = function () {
+    var userguess = String.fromCharCode(event.keyCode).toLocaleLowerCase();
+    console.log(userguess);
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = letters[Math.floor(Math.random()*letters.lenght)];
+
+     // for (var i = 0; i < 10; i++) {
+     //   computerGuess = letters[Math.floor(Math.random() * i)]; 
+
+    console.log(computerGuess);
+// compare USer Guess and Computer random input === // Win/lose conditions:
+    if (userguess === computerGuess) {
+        wins++;
+        alert('you win: ' + wins);
+    } else {
+        losses++;
+        alert('you loose: ' + losses);
+        }
+
+    // each time the userclick (document.onkeyup) --GuessesLeft and useruguessnumb++
+
+    var html = '<h3>Press any key to start the game ;)</h3>' + '<p >Wins: ' + wins + '</p>' + '<p>Losses: ' + losses + '</p>' +
+        '<p>Guesses Left: ' + GuessesLeft + '</p>' + '<p>You guesses so far: ' + useruguessnumb + '</p>'
 }
-
